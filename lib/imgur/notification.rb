99806bcc5214018ea1ddcd511d2ba7aa922144c5
Ross Paffett <ross@rosspaffett.com>
2013-11-09 09:06:20 -0500

    Add defaults to notifications

--- lib/imgur/notification.rb
+++ lib/imgur/notification.rb
@@ -1,10 +1,15 @@
 module Imgur
   class Notification
+    DEFAULTS = {
+      sender: 'com.runningwithcrayons.Alfred-2',
+      title: 'Upload to Imgur'
+    }
+
     attr_reader :message, :options
 
     def initialize(message, options = {})
       @message = message
-      @options = options
+      @options = DEFAULTS.merge(options)
     end
 
     def notify
--- test/unit/notification_test.rb
+++ test/unit/notification_test.rb
@@ -3,19 +3,31 @@ require 'test_helper'
 module Imgur
   class NotificationTest < TestCase
     test 'creating a notification' do
-      options = { title: 'A title', open: 'http://example.com' }
+      options = { open: 'http://example.com', title: 'A title' }
       notification = Notification.new('A message', options)
 
+      expected_options = {
+        open: 'http://example.com',
+        sender: 'com.runningwithcrayons.Alfred-2',
+        title: 'A title'
+      }
+
       assert_equal 'A message', notification.message
-      assert_equal options, notification.options
+      assert_equal expected_options, notification.options
     end
 
     test 'sending a notification' do
       TerminalNotifier.stubs :notify
-      Notification.new('A message', title: 'A title', open: 'http://example.com').notify
+      Notification.new('A message', subtitle: 'A subtitle').notify
+
+      expected_options = {
+        sender: 'com.runningwithcrayons.Alfred-2',
+        subtitle: 'A subtitle',
+        title: 'Upload to Imgur'
+      }
 
       assert_received TerminalNotifier, :notify do |expect|
-        expect.with 'A message', title: 'A title', open: 'http://example.com'
+        expect.with 'A message', expected_options
       end
     end
   end
